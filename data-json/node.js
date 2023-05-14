5cd6511eb510da027a8e1a83a043fdb2802f206e
vishwesh91 <vishwesh@pi314.in>
2023-05-14 07:41:48 +0530

    node js added

--- data-json/node.js
+++ data-json/node.js
@@ -138,5 +138,285 @@ export const nodeJson =[
 		"updatedAt": "2023-05-13T13:19:36.667Z",
 		"publishedAt": "2023-05-13T13:19:36.665Z",
 		"readTime": 5
+	},
+	{
+		"id": 11,
+		"title": "Creating a RESTful API with Node.js and Express.js",
+		"slug": "creating-a-res-tful-api-with-node-js-and-express-js",
+		"metaDesc": "Are you interested in building a web application that allows other developers to communicate with it? If so, then you need to build a RESTful API. In this article, we will discuss how to create a RESTful API using Node.js and Express.js.",
+		"tags": [
+			"node js"
+		],
+		"img": "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"createdAt": "2023-05-13T18:40:22.432Z",
+		"updatedAt": "2023-05-13T18:40:24.215Z",
+		"publishedAt": "2023-05-13T18:40:24.213Z",
+		"readTime": 5
+	},
+	{
+		"id": 12,
+		"title": "10 Essential Node.js Express.js Middleware for Web Development",
+		"slug": "10-essential-node-js-express-js-middleware-for-web-development",
+		"metaDesc": "Middleware plays a crucial role in web development. It is the layer that sits between the web application and the server, helping to streamline the process and make the application run more efficiently. In Node.js and Express.js, middleware is used to add functionality to the application, and there are many different types of middleware available. In this article, we will take a look at 10 essential Node.js Express.js middleware that are crucial for web development.",
+		"tags": [
+			"node js"
+		],
+		"img": "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"createdAt": "2023-05-13T21:52:19.081Z",
+		"updatedAt": "2023-05-13T21:52:20.474Z",
+		"publishedAt": "2023-05-13T21:52:20.470Z",
+		"readTime": 7
+	},
+	{
+		"id": 13,
+		"title": "How to Build a CRUD Application with Node.js and Express.js",
+		"slug": "how-to-build-a-crud-application-with-node-js-and-express-js",
+		"metaDesc": "If you are new to Node.js and Express.js, you might be wondering how to build a CRUD application with these technologies. In this tutorial, we will walk you through the steps to create a CRUD application using Node.js and Express.js.",
+		"tags": [
+			"node js"
+		],
+		"img": "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"createdAt": "2023-05-13T22:02:09.619Z",
+		"updatedAt": "2023-05-13T22:02:10.976Z",
+		"publishedAt": "2023-05-13T22:02:10.973Z",
+		"readTime": 7
+	},
+	{
+		"id": 14,
+		"title": "Debugging Node.js Express.js Applications with Visual Studio Code",
+		"slug": "debugging-node-js-express-js-applications-with-visual-studio-code",
+		"metaDesc": "If you're a Node.js developer, you know how important it is to be able to debug your applications effectively. Debugging can be a challenging task, especially when you're dealing with complex applications. Fortunately, Visual Studio Code (VS Code) provides powerful debugging tools that can make your life a lot easier. In this article, we'll take a look at how to debug Node.js Express.js applications with VS Code.",
+		"tags": [
+			"node js"
+		],
+		"img": "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"createdAt": "2023-05-13T22:09:56.402Z",
+		"updatedAt": "2023-05-13T22:09:57.652Z",
+		"publishedAt": "2023-05-13T22:09:57.649Z",
+		"readTime": 4
+	},
+	{
+		"id": 15,
+		"title": "How to Use JWT Authentication with Node.js Express.js",
+		"slug": "how-to-use-jwt-authentication-with-node-js-express-js",
+		"metaDesc": "JWT (JSON Web Token) is a popular authentication mechanism that is widely used in modern web applications. It is a JSON-based open standard that allows transmitting data between parties securely. JWT authentication is easy to implement and can be used to authenticate users in a Node.js and Express.js application.",
+		"tags": [
+			"node js"
+		],
+		"img": "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"createdAt": "2023-05-13T22:14:58.325Z",
+		"updatedAt": "2023-05-13T22:14:59.631Z",
+		"publishedAt": "2023-05-13T22:14:59.629Z",
+		"readTime": 5
+	},
+	{
+		"id": 16,
+		"title": "Optimizing Node.js Express.js Performance for High Traffic",
+		"slug": "optimizing-node-js-express-js-performance-for-high-traffic",
+		"metaDesc": "Node.js and Express.js are popular frameworks for building fast, scalable web applications. However, as your application grows and starts receiving high traffic, it's important to optimize its performance to ensure a smooth user experience. In this article, we'll explore some best practices for optimizing Node.js and Express.js performance for high traffic.",
+		"tags": [
+			"node js"
+		],
+		"img": "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"createdAt": "2023-05-13T22:20:27.073Z",
+		"updatedAt": "2023-05-13T22:20:28.293Z",
+		"publishedAt": "2023-05-13T22:20:28.291Z",
+		"readTime": 4
+	},
+	{
+		"id": 17,
+		"title": "Node.js Express.js Best Practices for Error Handling",
+		"slug": "node-js-express-js-best-practices-for-error-handling",
+		"metaDesc": "Error handling is an essential part of any Node.js and Express.js application development. In this article, we'll discuss some of the best practices for error handling in Node.js and Express.js. We'll cover how to handle different types of errors, how to log errors, and how to use middleware for error handling. So let's get started!",
+		"tags": [
+			"node js"
+		],
+		"img": "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"createdAt": "2023-05-13T22:23:14.839Z",
+		"updatedAt": "2023-05-13T22:23:16.158Z",
+		"publishedAt": "2023-05-13T22:23:16.156Z",
+		"readTime": 4
+	},
+	{
+		"id": 18,
+		"title": "How to Unit Test Node.js Express.js Applications with Jest",
+		"slug": "how-to-unit-test-node-js-express-js-applications-with-jest",
+		"metaDesc": "If you are building web applications with Node.js and Express.js, it is essential to write unit tests to ensure your code is working correctly. Unit tests help you to catch errors and bugs before they make it to production, allowing you to deliver high-quality code with confidence. Jest is a popular testing framework that is easy to set up and use, making it an excellent choice for testing your Node.js and Express.js applications.\n\nIn this article, we will cover how to set up and write unit tests for your Node.js Express.js application using Jest.",
+		"tags": [
+			"node js"
+		],
+		"img": "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"createdAt": "2023-05-13T22:30:22.647Z",
+		"updatedAt": "2023-05-13T22:30:23.917Z",
+		"publishedAt": "2023-05-13T22:30:23.915Z",
+		"readTime": 4
+	},
+	{
+		"id": 19,
+		"title": "Building Real-time Applications with Node.js, Express.js, and Socket.io",
+		"slug": "building-real-time-applications-with-node-js-express-js-and-socket-io",
+		"metaDesc": "In the world of web development, real-time applications are becoming increasingly popular. They allow users to experience the instant gratification of interacting with applications that respond in real-time. Node.js, Express.js, and Socket.io are three technologies that are essential for building real-time applications. In this article, we'll discuss how to use these technologies together to create real-time applications.",
+		"tags": [
+			"node js"
+		],
+		"img": "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"createdAt": "2023-05-13T23:12:52.084Z",
+		"updatedAt": "2023-05-13T23:12:53.360Z",
+		"publishedAt": "2023-05-13T23:12:53.358Z",
+		"readTime": 7
+	},
+	{
+		"id": 20,
+		"title": "Node.js vs. Express.js: Which One to Choose for Your Project?",
+		"slug": "node-js-vs-express-js-which-one-to-choose-for-your-project",
+		"metaDesc": "Node.js and Express.js are both popular technologies used for server-side development. Node.js is a runtime environment that allows developers to run JavaScript on the server-side, while Express.js is a framework built on top of Node.js that provides a streamlined approach to building web applications.\n\nIn this article, we will explore the differences between Node.js and Express.js, and help you decide which one is best for your project.",
+		"tags": [
+			"node js"
+		],
+		"img": "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"createdAt": "2023-05-13T23:54:22.062Z",
+		"updatedAt": "2023-05-13T23:54:23.281Z",
+		"publishedAt": "2023-05-13T23:54:23.278Z",
+		"readTime": 4
+	},
+	{
+		"id": 21,
+		"title": "Getting Started with Node.js and MongoDB",
+		"slug": "getting-started-with-node-js-and-mongo-db",
+		"metaDesc": "If you are interested in developing web applications, Node.js and MongoDB are two essential technologies you should learn. Node.js is a powerful and flexible server-side platform that allows you to write server-side code using JavaScript, while MongoDB is a popular NoSQL database that provides a flexible and scalable way to store and manage your data. In this article, we will introduce you to Node.js and MongoDB, and show you how to get started with them.",
+		"tags": [
+			"node js"
+		],
+		"img": "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"createdAt": "2023-05-13T23:57:56.170Z",
+		"updatedAt": "2023-05-13T23:57:57.353Z",
+		"publishedAt": "2023-05-13T23:57:57.350Z",
+		"readTime": 5
+	},
+	{
+		"id": 22,
+		"title": "10 Tips for Optimizing Node.js and MongoDB Performance",
+		"slug": "10-tips-for-optimizing-node-js-and-mongo-db-performance",
+		"metaDesc": "Node.js and MongoDB are two powerful technologies used for developing modern web applications. Node.js is a server-side JavaScript runtime, while MongoDB is a NoSQL database designed to store and manage large volumes of data. When used together, Node.js and MongoDB can provide a powerful and scalable platform for building high-performance web applications.\n\nHowever, to ensure optimal performance, it's important to follow best practices when developing and deploying Node.js and MongoDB applications. In this article, we will provide 10 tips for optimizing Node.js and MongoDB performance.",
+		"tags": [
+			"node js"
+		],
+		"img": "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"createdAt": "2023-05-14T00:00:31.676Z",
+		"updatedAt": "2023-05-14T00:00:32.959Z",
+		"publishedAt": "2023-05-14T00:00:32.957Z",
+		"readTime": 4
+	},
+	{
+		"id": 23,
+		"title": "How to Use Mongoose with Node.js and MongoDB",
+		"slug": "how-to-use-mongoose-with-node-js-and-mongo-db",
+		"metaDesc": "Mongoose is an Object Data Modeling (ODM) library for Node.js that provides a way to interact with MongoDB databases. It allows you to define data schemas, perform queries, and provides other helpful features that make it easier to work with MongoDB.\n\nIn this article, we'll go through the steps of installing and setting up Mongoose, defining a schema, performing basic CRUD operations, and using some of Mongoose's other features.",
+		"tags": [
+			"node js"
+		],
+		"img": "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"createdAt": "2023-05-14T00:09:21.821Z",
+		"updatedAt": "2023-05-14T00:09:23.167Z",
+		"publishedAt": "2023-05-14T00:09:23.164Z",
+		"readTime": 7
+	},
+	{
+		"id": 24,
+		"title": "Node.js MongoDB Transactions: Best Practices for Handling Data",
+		"slug": "node-js-mongo-db-transactions-best-practices-for-handling-data",
+		"metaDesc": "MongoDB is a popular document-oriented NoSQL database that provides flexibility in handling unstructured data. It's widely used in modern web applications because of its ability to scale horizontally and support high throughput. However, when it comes to handling data, transactions are crucial to ensure consistency and reliability in your application. In this article, we'll explore the best practices for handling data with Node.js and MongoDB transactions.",
+		"tags": [
+			"node js"
+		],
+		"img": "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"createdAt": "2023-05-14T00:10:16.934Z",
+		"updatedAt": "2023-05-14T00:24:16.098Z",
+		"publishedAt": "2023-05-14T00:24:16.096Z",
+		"readTime": 5
+	},
+	{
+		"id": 25,
+		"title": "Node.js MongoDB CRUD Operations: A Step-by-Step Guide",
+		"slug": "node-js-mongo-db-crud-operations-a-step-by-step-guide",
+		"metaDesc": "If you are a web developer looking to build a Node.js application that requires database functionality, MongoDB is a popular choice. MongoDB is a NoSQL database that stores data in JSON-like documents, making it a flexible and scalable option for modern web applications.\n\nIn this tutorial, we will walk through the basic CRUD (Create, Read, Update, Delete) operations in MongoDB using Node.js. We will cover how to connect to a MongoDB database, create documents, retrieve documents, update documents, and delete documents.",
+		"tags": [
+			"node js"
+		],
+		"img": "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"createdAt": "2023-05-14T00:13:56.171Z",
+		"updatedAt": "2023-05-14T00:13:57.396Z",
+		"publishedAt": "2023-05-14T00:13:57.394Z",
+		"readTime": 4
+	},
+	{
+		"id": 26,
+		"title": "Node.js MongoDB Security: Best Practices for Authentication and Authorization",
+		"slug": "node-js-mongo-db-security-best-practices-for-authentication-and-authorization",
+		"metaDesc": "When building web applications with Node.js and MongoDB, security should always be a top priority. In this article, we will cover the best practices for authentication and authorization in Node.js and MongoDB.",
+		"tags": [
+			"node js"
+		],
+		"img": "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"createdAt": "2023-05-14T00:28:55.883Z",
+		"updatedAt": "2023-05-14T00:28:57.107Z",
+		"publishedAt": "2023-05-14T00:28:57.105Z",
+		"readTime": 5
+	},
+	{
+		"id": 27,
+		"title": "Debugging Node.js and MongoDB Applications with the Node.js Driver",
+		"slug": "debugging-node-js-and-mongo-db-applications-with-the-node-js-driver",
+		"metaDesc": "If you are developing Node.js applications that interact with MongoDB, you might run into issues or errors while coding or deploying. Debugging such applications can be a challenging task, especially for beginners. However, the Node.js driver for MongoDB provides a rich set of debugging tools that can help you diagnose and resolve issues in your code. In this article, we will explore some of the techniques you can use to debug Node.js and MongoDB applications using the Node.js driver.",
+		"tags": [
+			"node js"
+		],
+		"img": "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"createdAt": "2023-05-14T00:34:11.337Z",
+		"updatedAt": "2023-05-14T00:34:12.618Z",
+		"publishedAt": "2023-05-14T00:34:12.617Z",
+		"readTime": 6
+	},
+	{
+		"id": 28,
+		"title": "How to Build Real-time Applications with Node.js and MongoDB Change Streams",
+		"slug": "how-to-build-real-time-applications-with-node-js-and-mongo-db-change-streams",
+		"metaDesc": "Node.js and MongoDB are two popular technologies used for building real-time applications. While Node.js is a server-side JavaScript runtime environment, MongoDB is a NoSQL document-oriented database. MongoDB Change Streams is a feature that enables real-time data processing and notifications for MongoDB collections. In this tutorial, we will explore how to build real-time applications with Node.js and MongoDB Change Streams.",
+		"tags": [
+			"node js"
+		],
+		"img": "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"createdAt": "2023-05-14T01:50:56.518Z",
+		"updatedAt": "2023-05-14T01:50:57.689Z",
+		"publishedAt": "2023-05-14T01:50:57.687Z",
+		"readTime": 4
+	},
+	{
+		"id": 29,
+		"title": "Node.js and MongoDB Atlas: How to Build a Cloud-Native Application",
+		"slug": "node-js-and-mongo-db-atlas-how-to-build-a-cloud-native-application",
+		"metaDesc": "In the world of modern web development, building cloud-native applications has become increasingly popular. A cloud-native application is one that is designed specifically to run in a cloud computing environment, with the aim of taking full advantage of the flexibility and scalability that such an environment provides. In this article, we'll explore how to build a cloud-native application using Node.js and MongoDB Atlas.",
+		"tags": [
+			"node js"
+		],
+		"img": "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"createdAt": "2023-05-14T01:56:09.048Z",
+		"updatedAt": "2023-05-14T01:56:10.415Z",
+		"publishedAt": "2023-05-14T01:56:10.413Z",
+		"readTime": 5
+	},
+	{
+		"id": 30,
+		"title": "How to Use GraphQL with Node.js and MongoDB",
+		"slug": "how-to-use-graph-ql-with-node-js-and-mongo-db",
+		"metaDesc": "GraphQL is a query language that enables developers to request specific data from APIs in a more efficient and flexible way than traditional REST APIs. Node.js is a popular server-side JavaScript runtime, and MongoDB is a NoSQL database that stores data in JSON-like documents. In this tutorial, we will walk through how to use GraphQL with Node.js and MongoDB to build a powerful and scalable backend for your web or mobile applications.",
+		"tags": [
+			"node js"
+		],
+		"img": "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		"createdAt": "2023-05-14T02:04:29.144Z",
+		"updatedAt": "2023-05-14T02:04:30.762Z",
+		"publishedAt": "2023-05-14T02:04:30.760Z",
+		"readTime": 6
 	}
 ]
