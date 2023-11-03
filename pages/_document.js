f77e8bc1ff833ce49e6382bf560c0e0f82778868
vishwesh91 <vishwesh@pi314.in>
2023-11-04 00:05:40 +0530

    minor chanhge

--- pages/_document.js
+++ pages/_document.js
@@ -5,11 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3199347075507532"
-          crossorigin="anonymous"
-        />
+       
         <link rel="icon" href="/favicon.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
